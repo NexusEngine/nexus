221dd0bdc4f11f9586bb771d6bc8bd5a4d0fc504
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-11-26 22:34:46 +0100

    start config

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,1 +1,6 @@
-//
+import type { Schema } from './config'
+import { pathToFileURL } from 'url'
+import { readFile } from 'fs/promises'
+import { load } from 'js-yaml'
+
+export const configPath = new URL('.engine.yaml', `${pathToFileURL(process.cwd())}/`)
