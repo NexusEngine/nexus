8e81667ff160eee3ae668747447a6db1a7171d93
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-11-28 21:22:01 +0100

    remove config as separate package

--- packages/config/src/index.ts
+++
@@ -1,6 +0,0 @@
-import type { Schema } from './config'
-import { pathToFileURL } from 'url'
-import { readFile } from 'fs/promises'
-import { load } from 'js-yaml'
-
-export const configPath = new URL('.engine.yaml', `${pathToFileURL(process.cwd())}/`)
