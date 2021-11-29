f1e9a0f650bdf659d8822e29e6dd017d092ea2b7
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-11-29 23:09:51 +0100

    mod import error handling

--- packages/engine/src/config/mods.ts
+++ packages/engine/src/config/mods.ts
@@ -1,28 +1,54 @@
 import config from '../config/index.js'
 
 export type Provide = 'storage' | 'config'
+
+/**
+ * A mod manifest presents metadata to the engine.
+ * Each mod must have a manifest present in its main file.
+ *
+ * @example
+ *
+ * import type { Manifest } from '@nexus-engine/engine'
+ * export const manifest: Manifest = {
+ *    provides: ['storage', 'config'],
+ * }
+ */
 export type Manifest = {
+  /**
+   * Keys which this mod provides to the engine.
+   * A mod can provide zero, one, or more keys simultaneously.
+   *
+   * The appropriate files are only imported when necessary.
+   */
   provides: Provide | Provide[],
 }
 
 const mods: {
   specifier: string,
   manifest: Manifest,
-}[] = []
-async function resolve(specifiers: string[]) {
-  mods.push(...await Promise.all(specifiers.map(async specifier => {
-    const manifest = (await import(specifier)).manifest as Manifest
-    return { specifier, manifest }
-  })))
-}
-await resolve(config.mods ?? [])
+}[] = await Promise.all((config.mods ?? []).map(async specifier => {
+  const manifest = (await import(specifier)).manifest as Manifest
+  return { specifier, manifest }
+}))
 
 export { mods }
+
+/**
+ * Import mods for the given provide key.
+ * @param provide The provide key
+ */
 export async function importMods(provide: Provide) {
   for (const { specifier, manifest } of mods) {
     if (manifest.provides === provide || manifest.provides.includes(provide)) {
-      // TODO: This doesn't work yet
-      await import(`${specifier}/dist/${provide}.js`)
+      try {
+        await import(`${specifier}/dist/${provide}.js`)
+      } catch (err: any) {
+        if (err.code === 'ERR_MODULE_NOT_FOUND') {
+          console.log(`Failed to import mod "${specifier}/${provide}". Continuing without.`)
+        } else {
+          throw err
+        }
+      }
     }
   }
 }
