eb5877115e00ec14c377f1f3075b7c32ec2d47cf
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-11-29 19:01:00 +0100

    start exporting mods api

--- packages/engine/src/export.ts
+++ packages/engine/src/export.ts
@@ -1,2 +1,13 @@
-// here all interfaces are exported
-export const meaningOfLife = 42
+
+/**
+ * This is the main export file which gets imported
+ * when you import the engine itself.
+ *
+ * All exported members are part of the Mods API.
+ */
+
+export type { Schema } from './config/config'
+
+export { default as config } from './config/index.js'
+export { GameObject } from './objects/GameObject.js'
+export { registerProvider, buildProvider } from './storage/provider.js'
