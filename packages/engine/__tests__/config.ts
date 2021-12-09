54b0fcc2947fd29458ff296ce7bf7ad761408124
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-12-09 17:12:28 +0100

    getting tests to work

--- packages/engine/__tests__/config.ts
+++ packages/engine/__tests__/config.ts
@@ -1,5 +1,7 @@
-export {}
+import { config } from '../dist/config/index.js'
 
 describe('test 1', () => {
-  test('should pass', () => {})
+  test('should pass', () => {
+    config()
+  })
 })
--- packages/nexusctl/__tests__/config.ts
+++ packages/nexusctl/__tests__/config.ts
@@ -1,5 +1,11 @@
-export {}
+import { readFileSync } from 'fs'
+import { load } from 'js-yaml'
 
 describe('test 1', () => {
-  test('should pass', () => {})
+  test('should pass', () => {
+    const text = readFileSync('.nexus.yml', 'utf-8')
+    const content = load(text) as any
+    expect(content).toHaveProperty('shard')
+    expect(content).toHaveProperty('storage')
+  })
 })
