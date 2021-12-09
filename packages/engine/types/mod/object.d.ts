dcd066d7d6fca3cc6c53f95c66aecd994378d10b
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-12-09 22:07:43 +0100

    add jsdoc comments

--- packages/engine/types/mod/object.d.ts
+++ packages/engine/types/mod/object.d.ts
@@ -1,3 +1,8 @@
+/**
+ * Represents a base game object.
+ * Game objects provide the logic for your game.
+ * Almost anything can be a game object.
+ */
 declare abstract class GameObject<Shape extends BaseShape> {
   readonly ['#dbName']: string | null
   readonly ['#collectionName']: string
@@ -7,8 +12,23 @@ declare abstract class GameObject<Shape extends BaseShape> {
   constructor(dbName: string | null, collectionName: string, data: Shape)
   constructor(dbOrData: Shape | string | null, collectionName?: string, data?: Shape)
 
+  /**
+   * The ID of the game object.
+   */
   get id(): Shape['_id']
+
+  /**
+   * Flush the mutations to the shape to persistent storage.
+   */
   flush(): Promise<void>
+
+  /**
+   * Revoke the shape observer proxy, preparing it for garbage collection.
+   */
   revoke(): void
+
+  /**
+   * Flush mutations to persistent storage and revoke the observer proxy.
+   */
   flushAndRevoke(): Promise<void>
 }
