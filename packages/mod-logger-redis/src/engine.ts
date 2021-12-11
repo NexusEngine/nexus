309fb35702a37b4a8c6ad73ac9afbe2e439be066
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-12-11 20:13:19 +0100

    remove redis logger for now

--- packages/mod-logger-redis/src/engine.ts
+++
@@ -1,8 +0,0 @@
-import { RedisLogger } from './lib/logger'
-
-if (Engine.config.logger?.path?.startsWith('redis:')) {
-  Engine.register('preInitializer', async () => {
-    await Engine.log.close()
-    Engine.log = new RedisLogger(Engine.config.logger.path)
-  })
-}
