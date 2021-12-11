973d7ecaa3f51e8ea4291f4b915e50b4d5287756
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-12-11 20:02:27 +0100

    remove unusued globals

--- packages/engine/src/globals/index.ts
+++ packages/engine/src/globals/index.ts
@@ -1,6 +1,5 @@
 import { nanoid } from 'nanoid/async'
 import { registerGlobal } from '../utility/global'
-import { Processors } from '../game/symbols'
 
 import './engine'
 import './shard'
@@ -9,18 +8,7 @@ import './store'
 import './memory'
 import './stream'
 
-registerGlobal('Processors', Processors)
-
 // Register ID generator
 registerGlobal(async function generateId(size = 21) {
   return nanoid(size)
-})
-
-// Register intent error
-registerGlobal(class IntentError extends Error {
-  readonly code: string
-  constructor(code: string, message?: string) {
-    super(message)
-    this.code = code
-  }
 })
